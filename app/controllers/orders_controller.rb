14fe146cccf93761118410fe412f491b3a1abb7e
shiratama317 <shiratamax6x5@gmail.com>
2024-03-04 16:49:25 +0900

    モデル・コントローラーの作成、フォーム情報の保存

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,24 @@
+class OrdersController < ApplicationController
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipalities, :block_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id] )
+  end
+end
